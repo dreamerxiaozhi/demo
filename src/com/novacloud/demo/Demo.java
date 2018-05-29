61b424850aa1abbce413899a96c4c2983b672ee0
zhangxiaozhi <dreamxiaozhi@sina.com>
2018-05-29 13:22:38 +0800

    aaa

--- src/com/novacloud/demo/Demo.java
+++ src/com/novacloud/demo/Demo.java
@@ -4,6 +4,7 @@ package com.novacloud.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello world");
 		System.out.println("hello world");
     }
 }
